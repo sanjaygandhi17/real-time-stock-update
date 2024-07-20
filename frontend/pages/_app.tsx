84eed089bf073122d2249dfb3f01a4531dadf845
sanjaygandhi17 <71930174+sanjaygandhi17@users.noreply.github.com>
2024-07-20 17:43:35 +0530

    Update _app.tsx

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,31 +1,15 @@
-import React, { FC } from 'react';
+import type { AppProps } from 'next/app';
+import { Provider } from 'react-redux';
+import { store } from '../store';
+import React from 'react';
+import './styles.css'
 
-interface ModalProps {
-  isOpen: boolean;
-  onClose: () => void;
-  onSelect: (symbol: string) => void;
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
-const Modal: FC<ModalProps> = ({ isOpen, onClose, onSelect }) => {
-  if (!isOpen) return null;
-
-  return (
-    <div className="modal">
-      <div className="modal-content">
-        <div className="modal-header">
-          <h2>Select Stock/Crypto</h2>
-          <button className="close-button" onClick={onClose}>Close</button>
-        </div>
-        <div className="modal-buttons">
-          <button onClick={() => onSelect('dogecoin')}>Dogecoin</button>
-          <button onClick={() => onSelect('bitcoin')}>Bitcoin</button>
-          <button onClick={() => onSelect('ethereum')}>Ethereum</button>
-          <button onClick={() => onSelect('ripple')}>Ripple</button>
-          <button onClick={() => onSelect('litecoin')}>Litecoin</button>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default Modal;
+export default MyApp;
